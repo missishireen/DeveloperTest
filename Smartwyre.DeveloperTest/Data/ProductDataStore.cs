c9ad87674c5da49f916ab2b9ad5f6a7a5bc5bfc8
missishireen <shereenmessi@hotmail.com>
2024-03-15 10:34:43 +0000

    Add sample data for testing

--- Smartwyre.DeveloperTest/Data/ProductDataStore.cs
+++ Smartwyre.DeveloperTest/Data/ProductDataStore.cs
@@ -1,12 +1,36 @@
 ﻿using Smartwyre.DeveloperTest.Types;
+using System.Collections.Generic;
 
 namespace Smartwyre.DeveloperTest.Data;
 
-public class ProductDataStore:IProductDataStore
+public class ProductDataStore : IProductDataStore
 {
+    // This is just to allow manual testing
+    private readonly Dictionary<string, Product> _productData;
+
+    public ProductDataStore()
+    {
+        _productData = new Dictionary<string, Product>();
+        InitializeSampleData();
+    }
+
+    private void InitializeSampleData()
+    {
+        _productData.Add("1000", new Product { Identifier = "1000", Price = 10, SupportedIncentives = SupportedIncentiveType.FixedRateRebate });
+        _productData.Add("1001", new Product { Identifier = "1001", Price = 100, SupportedIncentives = SupportedIncentiveType.AmountPerUom });
+        _productData.Add("1002", new Product { Identifier = "1002", Price = 15, SupportedIncentives = SupportedIncentiveType.FixedCashAmount });
+
+    }
+
     public Product GetProduct(string productIdentifier)
     {
-        // Access database to retrieve account, code removed for brevity 
-        return new Product();
+        if (_productData.ContainsKey(productIdentifier))
+        {
+            return _productData[productIdentifier];
+        }
+        else
+        {
+            return null;
+        }
     }
 }
--- Smartwyre.DeveloperTest/Data/RebateDataStore.cs
+++ Smartwyre.DeveloperTest/Data/RebateDataStore.cs
@@ -1,13 +1,49 @@
 ﻿using Smartwyre.DeveloperTest.Types;
+using System;
+using System.Collections.Generic;
 
 namespace Smartwyre.DeveloperTest.Data;
 
-public class RebateDataStore:IRebateDataStore
+public class RebateDataStore : IRebateDataStore
 {
+    // This is just to allow manual testing
+    private readonly Dictionary<string, Rebate> _rebateData;
+
+    public RebateDataStore()
+    {
+        _rebateData = new Dictionary<string, Rebate>();
+        InitializeSampleData();
+    }
+
+    private void InitializeSampleData()
+    {
+        _rebateData.Add("12345", new Rebate
+        {
+            Identifier = "12345",
+            Incentive = IncentiveType.FixedCashAmount,
+            Amount = 50
+        });
+
+        _rebateData.Add("54333", new Rebate
+        {
+            Identifier = "54333",
+            Incentive = IncentiveType.FixedRateRebate,
+            Percentage = 0.1m // 10%
+        });
+    }
+
     public Rebate GetRebate(string rebateIdentifier)
     {
         // Access database to retrieve account, code removed for brevity 
-        return new Rebate();
+
+        if (_rebateData.ContainsKey(rebateIdentifier))
+        {
+            return _rebateData[rebateIdentifier];
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public void StoreCalculationResult(Rebate account, decimal rebateAmount)
